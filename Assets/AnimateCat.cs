1848909b5b9bd47cc443ca621ca507dce62c83a7
Unknown <rfonger@gmail.com>
2020-05-11 23:33:03 -0600

    Adding 2 player controls

---
+++ Assets/AnimateCat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateCat : MonoBehaviour
+{
+    public float loopTime = 1f;
+    // Update is called once per frame
+
+    private float loopTimer = 0f;
+
+    void Update()
+    {
+        loopTimer += Time.deltaTime;
+
+        if (loopTimer >= loopTime){
+            loopTimer = 0f;
+            Vector3 scale = transform.localScale;
+            scale.x = -scale.x;
+            transform.localScale = scale;
+        }
+        
+    }
+}
--- Assets/Resources/Scripts/PlayerInput.cs
+++ Assets/Resources/Scripts/PlayerInput.cs
@@ -6,11 +6,17 @@ public class PlayerInput : MonoBehaviour
 {
 
     public float speed = 1f;
+    public int playerNumber = 1;
+
     // Update is called once per frame
     void Update()
     {
-        float moveY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        string verticalAxisName = "P" + playerNumber + "_Vertical";
+        string horizontalAxisName = "P" + playerNumber + "_Horizontal";
+
+
+        float moveY = Input.GetAxis(verticalAxisName) * Time.deltaTime * speed;
+        float moveX = Input.GetAxis(horizontalAxisName) * Time.deltaTime * speed;
 
         transform.Translate(moveX,moveY,0);
 
