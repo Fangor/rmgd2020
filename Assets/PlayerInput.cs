43b2fc616acc2c73cb2e83bc0e2dc58102e1b584
Unknown <rfonger@gmail.com>
2020-05-11 22:26:48 -0600

    Adding controls

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public float speed = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        float moveY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        transform.Translate(moveX,moveY,0);
+
+
+    }
+}
