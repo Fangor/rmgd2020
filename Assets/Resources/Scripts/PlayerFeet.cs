c2be27e858c5593a86bc932444d2f7212d95e755
Unknown <rfonger@gmail.com>
2020-05-16 11:11:32 -0600

    Better ground detection

--- Assets/Resources/Scripts/PlayerFeet.cs
+++ Assets/Resources/Scripts/PlayerFeet.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerFeet : MonoBehaviour
 {
     public bool touchingGround = false;
+
+    private HashSet<Collider2D> groundCollidersImTouching = new HashSet<Collider2D>();
     private void OnTriggerEnter2D(Collider2D otherCollider){
         if ("ground".Equals(otherCollider.tag)){
             touchingGround = true;
@@ -12,7 +14,10 @@ public class PlayerFeet : MonoBehaviour
     }
 
     private void OnTriggerExit2D(Collider2D otherCollider){
-        if ("ground".Equals(otherCollider.tag)){
+        if (groundCollidersImTouching.Contains(otherCollider)){
+            groundCollidersImTouching.Remove(otherCollider);
+        }
+        if (groundCollidersImTouching.Count <= 0){
             touchingGround = false;
         }
     }
