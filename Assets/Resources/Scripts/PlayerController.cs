f8a5fee1c395d4423b08ab5a15cfc8cfeb3f811b
Unknown <rfonger@gmail.com>
2020-05-16 00:36:23 -0600

    Attempt at getting jumping working

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -7,11 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     private Vector2 i_movement;
     public float moveSpeed = 3f;
+    public float jumpForce = 1f;
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Move(){
+
         Vector2 movement = new Vector2(i_movement.x, i_movement.y).normalized * moveSpeed * Time.deltaTime;
         transform.Translate(movement);
     }
@@ -28,11 +32,23 @@ public class PlayerController : MonoBehaviour
     private void OnMove(InputValue value)
     {
         i_movement = value.Get<Vector2>();
+        if (i_movement.x > 0f){
+            i_movement.x = 1f;
+        } else if (i_movement.x < 0f){
+            i_movement.x = -1f;
+        } else {
+            i_movement.x = 0f;
+        }
+        i_movement.y = 0f;
     }
 
     private void OnButtonSouth()
     {
-        //Attack/defend
+        //Jump
+        PlayerFeet playerFeet = GetComponentInChildren<PlayerFeet>();
+        if (playerFeet.touchingGround){
+            rb.AddForce(Vector2.up * jumpForce);
+        }
     }
     private void OnButtonEast()
     {
---
+++ Assets/Resources/Scripts/PlayerFeet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFeet : MonoBehaviour
+{
+    public bool touchingGround = false;
+    private void OnTriggerEnter(Collider otherCollider){
+        if ("ground".Equals(otherCollider.tag)){
+            touchingGround = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider otherCollider){
+        touchingGround = false;
+    }
+}
